1434037adb80800a6a208c8662a5541abeb7b117
Edith <edithbanda014@gmail.com>
2023-02-17 15:01:10 -0800

    All possible combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: program that prints all possible different combinations of two digits
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
  * Return: Always 0 (success)
  */
 
