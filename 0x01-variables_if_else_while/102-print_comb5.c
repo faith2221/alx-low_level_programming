01beff635ebac4302563e2294a3e20c3dd9e5c53
Edith <edithbanda014@gmail.com>
2023-02-20 01:14:46 -0800

    Prints all possible two digit combinations from 0 to 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,14 +12,14 @@ int main(void)
 	int c = 0;
 	int f_d;
 	int l_d;
-	
+
 	int c2;
 	int f_d2;
 	int l_d2;
 
 	while (c <= 98)
 	{
-		f_d = (c / 10 + '0');
+		f_d = (c /10 +'0');
 		l_d = (c % 10 + '0');
 		c2 = 0;
 		while (c2 <= 99)
@@ -47,4 +47,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}		
+}
