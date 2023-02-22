0e1d6786b77f637bebe7d7a53f697071be6194c5
Edith <edithbanda014@gmail.com>
2023-02-22 00:02:39 -0800

    Function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * _isalphat - show 1 if the input is a
- * letter another cases, show 0
+ * _isalphat - checks for alphabetic charecter
  *
  * @c: The character in ASCII code
  * Returns: 1 for letters. 0 for the rest
@@ -9,10 +8,13 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c<= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
 	else
+	{
 		return (0);
+	}
+	putchar('\n')
 }
