0605916ae430ee842c73a79969154c3436e6065a
Edith <edithbanda014@gmail.com>
2023-02-21 21:52:21 -0800

    Function that checks the lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,11 +4,16 @@
  * @c: a character argument
  * Returns: returns 1 and 0 depending on condition
  */
+
 int _islower(int c)
 {
-       if(c >= 97 && c <= 122);
-       {
-       return (1);
-       }
-       return (0);
-}     
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
