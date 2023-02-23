68d5e252bb54154062f39262e6fa5908b30a273a
Edith <edithbanda014@gmail.com>
2023-02-23 11:55:53 -0800

    Function that prints uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks for an uppercase letter
+ * @c: The number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
