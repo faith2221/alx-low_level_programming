3ea073f7962b7f86e69b4b265e4b466ef61a483c
Edith <edithbanda014@gmail.com>
2023-03-16 19:05:56 -0700

    Function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,8 +6,5 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-<<<<<<< HEAD
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-=======
->>>>>>> 94bb989aa6c091bac44ee31e0930b07831f9f8b2
 #endif
