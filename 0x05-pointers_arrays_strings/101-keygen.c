58b6167a1dcd7a7083057f791949c6990d1f1fff
faith2221 <edithbanda014@gmail.com>
2024-02-18 22:49:41 -0800

    corrections to task 10 & 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 /**
- * Main - Program that generates random valid  passwords 
- * for the program 101-crackme
+ * main - Program that generates random valid
+ * passwords for the program 101-crackme
  *
- * Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -22,9 +22,10 @@ int main(void)
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0')
+		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
+			sum += n;
 			putchar(n + '0');
 			break;
 		}
